418d2d580d95311028c9dc1a3dc7a03172531c09
Dave Syer <dsyer@pivotal.io>
2017-06-07 14:21:17 +0100

    Wait for neo to start

--- neo/run.sh
+++ neo/run.sh
@@ -1,5 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
 cd /var/lib/neo4j
 /docker-entrypoint.sh neo4j &
+end="$((SECONDS+30))"
+while true; do
+    [[ "200" = "$(curl --silent --write-out %{http_code} --output /dev/null http://localhost:7474)" ]] && break
+    [[ "${SECONDS}" -ge "${end}" ]] && exit 1
+    sleep 1
+done
 curl -v -u neo4j:neo4j -X POST localhost:7474/user/neo4j/password -H "Content-type:application/json" -d "{\"password\":\"secret\"}"
